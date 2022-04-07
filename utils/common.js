31d85ff4d68fd60db2fe66d04c2fc4566ee9c581
Roberto Ruiz <robertoruizibi@gmail.com>
2022-04-07 19:58:07 +0200

    feat(common): added common functions to use

---
+++ utils/common.js
@@ -0,0 +1,5 @@
+const isObjEmpty = (obj = {}) => Object.keys(obj).length === 0
+
+const isArrayEmpty = (array = []) => Array.isArray(array) && array.length === 0
+
+module.exports = { isObjEmpty, isArrayEmpty }
