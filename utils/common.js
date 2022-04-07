1b507d2b42bd99f52ca1f257ff8bbb808792fd7e
Roberto Ruiz <robertoruizibi@gmail.com>
2022-04-08 00:06:40 +0200

    feat(common): added new methods

--- utils/common.js
+++ utils/common.js
@@ -1,5 +1,25 @@
+/*
+Importacion de modulos
+*/
+const pool = require('../database/configdb');
+const bcrypt = require('bcryptjs');
+const _ = require("lodash");
+
+//---------------------------------------------------------------//
 const isObjEmpty = (obj = {}) => Object.keys(obj).length === 0
-
 const isArrayEmpty = (array = []) => Array.isArray(array) && array.length === 0
+const getFirstQueryValue = (value) => { return Object.values(value[0])[0] }
 
-module.exports = { isObjEmpty, isArrayEmpty }
+const checkEmailInBD = async (email) => {
+  const users_emails = await pool.query('SELECT email FROM `users`')
+  let userEmailsArray = []
+  users_emails.forEach(element => {userEmailsArray.push(element.email)});
+  return userEmailsArray.findIndex(emailArr => emailArr === email) !== -1
+}
+
+const checkPasswordInBD = async (password, email) => {
+  const user_password = await pool.query('SELECT password FROM `users` WHERE email = ?', [email])
+  return bcrypt.compareSync(password, getFirstQueryValue(user_password));
+}
+
+module.exports = { isObjEmpty, isArrayEmpty, checkEmailInBD, checkPasswordInBD }
